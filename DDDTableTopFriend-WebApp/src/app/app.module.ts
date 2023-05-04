0de3c0711781447fb1d60ab183b409b2580f9f2d
atos.pedro <atos.pedro@vegatech.com.br>
2023-05-04 12:38:03 -0300

    reorganizing the project and adding swagger

--- DDDTableTopFriend-WebApp/src/app/app.module.ts
+++ DDDTableTopFriend-WebApp/src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AuthenticationModule } from './features/authentication/authentication.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule,
-    CoreModule,
     AuthenticationModule
   ],
   providers: [],
--- DDDTableTopFriend.Api/Program.cs
+++ DDDTableTopFriend.Api/Program.cs
@@ -1,13 +1,40 @@
-using DDDTableTopFriend.Api;
+﻿using DDDTableTopFriend.Api;
 using DDDTableTopFriend.Application;
 using DDDTableTopFriend.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddPresenter()
         .AddApplication()
-        .AddInfrastructure(builder.Configuration);
+        .AddInfrastructure(builder.Configuration)
+        .AddSwaggerGen(option =>
+        {
+            option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            option.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id="Bearer"
+                        }
+                    },
+                    new string[]{}
+                }
+            });
+        });
 
     builder.Host
         .AddLogging();
@@ -17,6 +44,11 @@ var app = builder.Build();
 {
     app.Services.MigrateDatabase();
     app.UseHttpsRedirection();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Table Top Friend API V1"));
+    }
     app.AddLogging();
     app.UseAuthentication();
     app.UseAuthorization();
