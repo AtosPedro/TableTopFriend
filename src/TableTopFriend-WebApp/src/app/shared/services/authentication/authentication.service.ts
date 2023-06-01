6581ff253bbe22db6245f30d4c23e994df206d2d
atos.pedro <atos.pedro@vegatech.com.br>
2023-06-01 16:18:31 -0300

    a

--- src/TableTopFriend-WebApp/src/app/shared/services/authentication/authentication.service.ts
+++ src/TableTopFriend-WebApp/src/app/shared/services/authentication/authentication.service.ts
@@ -1,39 +1,40 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { AuthenticationResult } from './dtos/authenticationResult';
 import { RegisterRequest } from './dtos/registerRequest';
 import { LoginRequest } from './dtos/loginRequest';
+import { Result } from 'src/app/core/models/result';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthenticationService {
-  private loginUrl = environment.apiUrl + '/auth/login';
-  private registerUrl = environment.apiUrl + '/auth/register';
+  private serviceRoute = '/auth';
+  private loginUrl = environment.apiUrl + this.serviceRoute + '/login';
+  private registerUrl = environment.apiUrl + this.serviceRoute + '/register';
 
   constructor(private httpClient: HttpClient) { }
 
-  public register(registerRequest: RegisterRequest): AuthenticationResult | any {
-    let result: AuthenticationResult | any = null;
-
+  public register(registerRequest: RegisterRequest): Result<AuthenticationResult, HttpErrorResponse> {
+    let result: any = null;
     this.httpClient.post<AuthenticationResult>(this.registerUrl, registerRequest)
       .subscribe({
-        next: value => result = value,
-        error: err => result = err
+        next: value => result = Result.success<AuthenticationResult, HttpErrorResponse>(value),
+        error: err => result = Result.fail<AuthenticationResult, HttpErrorResponse>(err)
       });
 
     return result;
   }
 
-  public login(loginRequest: LoginRequest): AuthenticationResult | any {
-    let result: AuthenticationResult | any = null;
+  public login(loginRequest: LoginRequest): Result<AuthenticationResult, any> {
+    let result: any = null;
 
     this.httpClient.post<AuthenticationResult>(this.loginUrl, loginRequest)
       .subscribe({
-        next: value => result = value,
-        error: err => result = err
+        next: value => result = Result.success<AuthenticationResult, HttpErrorResponse>(value),
+        error: err => result = Result.fail<AuthenticationResult, HttpErrorResponse>(err)
       });
 
     return result;
